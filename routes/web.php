d837c20be0bb9d94b9bef50459511caba62c01b5
aungkyawkyawmyo_lammyanmar <aungkyaw@lammyanmar.com>
2018-09-04 17:53:02 +0630

    79

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-    // return new \App\Mail\ConfirmYourEmail();
-});
+Route::get('/', 'FrontendController@welcome');
 
 Route::get('/register/confirm/', 'ConfirmEmailController@index')->name('confirm-email');
 
